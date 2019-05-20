2d7558642e04d4a859b3fd4e3de034c328a690fc
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-05-20 21:43:30 +0200

    Update pgblitz.sh

--- pgblitz.sh
+++ pgblitz.sh
@@ -5,97 +5,83 @@
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
-source /opt/pgclone/functions/pgblitz.sh
-
-#starter
-#stasks
-
-# Outside Variables
-dlpath=$(cat /var/plexguide/server.hd.path)
 
 # Starting Actions
-
 touch /var/plexguide/logs/pgblitz.log
 
-# Inside Variables
-ls -la /opt/appdata/pgblitz/keys/processed | awk '{print $9}' | grep gdsa > /opt/appdata/plexguide/key.list
-keytotal=$(wc -l /opt/appdata/plexguide/key.list | awk '{ print $1 }')
-
-keyfirst=$(cat /opt/appdata/plexguide/key.list | head -n1)
-keylast=$(cat /opt/appdata/plexguide/key.list | tail -n1)
-
-keycurrent=0
-cyclecount=0
+# Repull excluded folder 
+ wget -qN https://raw.githubusercontent.com/PGBlitz/PGClone/v8.6/functions/exclude -P /opt/pgclone/functions/
 
 echo "" >> /var/plexguide/logs/pgblitz.log
 echo "" >> /var/plexguide/logs/pgblitz.log
 echo "----------------------------" >> /var/plexguide/logs/pgblitz.log
 echo "PG Blitz Log - First Startup" >> /var/plexguide/logs/pgblitz.log
+chown -R 1000:1000 "{{hdpath}}/downloads"
+chmod -R 775 "{{hdpath}}/downloads"
+chown -R 1000:1000 "{{hdpath}}/move"
+chmod -R 775 "{{hdpath}}/move"
+startscript () {
+while read p; do
 
-chown -R 1000:1000 "$dlpath/downloads"
-chmod -R 775 "$dlpath/downloads"
-chown -R 1000:1000 "$dlpath/move"
-chmod -R 775 "$dlpath/move"
+  cleaner="$(cat /var/plexguide/cloneclean)"
+  useragent="$(cat /var/plexguide/uagent)"
 
-while [ 1 ]; do
-
-  dlpath=$(cat /var/plexguide/server.hd.path)
-
-  # Permissions
-
-  if [ "$keylast" == "$keyuse" ]; then keycurrent=0; fi
-
-  let "keycurrent++"
-  keyuse=$(sed -n ''$keycurrent'p' < /opt/appdata/plexguide/key.list)
-
-  encheck=$(cat /var/plexguide/pgclone.transport)
-    if [ "$encheck" == "eblitz" ]; then
-    keytransfer="${keyuse}C"; else keytransfer="$keyuse"; fi
-
-let "cyclecount++"
+  let "cyclecount++"
   echo "----------------------------" >> /var/plexguide/logs/pgblitz.log
   echo "PG Blitz Log - Cycle $cyclecount" >> /var/plexguide/logs/pgblitz.log
   echo "" >> /var/plexguide/logs/pgblitz.log
-  echo "Utilizing: $keytransfer" >> /var/plexguide/logs/pgblitz.log
-  
-  rclone moveto "$dlpath/downloads/" "$dlpath/move/" \
+  echo "Utilizing: $p" >> /var/plexguide/logs/pgblitz.log
+
+  rclone moveto "{{hdpath}}/downloads/" "{{hdpath}}/move/" \
   --config /opt/appdata/plexguide/rclone.conf \
   --log-file=/var/plexguide/logs/pgblitz.log \
-  --log-level INFO --stats 5s --stats-file-name-length 0 \
+  --log-level ERROR --stats 5s --stats-file-name-length 0 \
   --min-age 2m \
-  --transfers=4 \
-  --checkers=8 \
   --fast-list \
   --update \
-  --exclude-from="/opt/appdata/pgblitz/exclude/exclude-file.txt"
+  --exclude-from="/opt/appdata/plexguide/exclude/exclude-file.txt"
 
-  chown -R 1000:1000 "$dlpath/move"
-  chmod -R 775 "$dlpath/move"
+  chown -R 1000:1000 "/mnt/move"
+  chmod -R 775 "/mnt/move"
 
-  rclone moveto "$dlpath/move/" "$keytransfer:/" \
+  rclone moveto "{{hdpath}}/move" "${p}{{encryptbit}}:/" \
   --config /opt/appdata/plexguide/rclone.conf \
   --log-file=/var/plexguide/logs/pgblitz.log \
   --log-level INFO --stats 5s --stats-file-name-length 0 \
-  --min-age 1m \
   --tpslimit 12 \
-  --checkers=8 \
-  --transfers=4 \
-  --update \
-  --user-agent="mycase" \
-  --fast-list \
-  --bwlimit 1000M \
+  --checkers=20 \
+  --transfers=16 \
+  --bwlimit {{bandwidth.stdout}}M \
   --max-size=300G \
-  --drive-chunk-size=128M \
-  --exclude-from="/opt/appdata/pgblitz/exclude/exclude-file.txt"
+  --user-agent="$useragent" \
+  --drive-chunk-size={{vfs_dcs}}M \
+  --exclude-from="/opt/appdata/plexguide/exclude/exclude-file.txt"
 
   echo "Cycle $cyclecount - Sleeping for 30 Seconds" >> /var/plexguide/logs/pgblitz.log
   cat /var/plexguide/logs/pgblitz.log | tail -200 > /var/plexguide/logs/pgblitz.log
+  #sed -i -e "/Duplicate directory found in destination/d" /var/plexguide/logs/pgblitz.log
+  sleep 30
+  echo "Cycle $cyclecount - Sleeping for 30 Seconds" >> /var/plexguide/logs/pgblitz.log
+  cat /var/plexguide/logs/pgblitz.log | tail -200 > /var/plexguide/logs/pgblitz.log
+  #sed -i -e "/Duplicate directory found in destination/d" /var/plexguide/logs/pgblitz.log
+  sleep 30
 
-  sleep 5
+  #Quick fix
+  # Remove empty directories
+  #find "$dlpath/downloads/" -mindepth 2 -type d -empty -exec rm -rf {} \;
+  #find "$dlpath/move/" -type d -empty -exec rm -rf {} \;
+  #find "$dlpath/move/" -mindepth 2 -type f -cmin +5 -size +1M -exec rm -rf {} \;
 
-# Remove empty directories
-find "$dlpath/downloads" -mindepth 2 -type d -empty -exec rm -rf {} \;
-find "$dlpath/move/" -type d -empty -exec rm -rf {} \;
-find "$dlpath/move/" -mindepth 2 -type f -cmin +5 -size +1M -exec rm -rf {} \;
+  # Remove empty directories
+  find "{{hdpath}}/move/" -mindepth 2 -type d -mmin +2 -empty -exec rm -rf {} \;
 
-done
+  # Removes garbage | torrent folder excluded 
+  find "{{hdpath}}/downloads" -mindepth 2 -type d -cmin +$cleaner  $(printf "! -name %s " $(cat /opt/pgclone/functions/exclude)) -empty -exec rm -rf {} \;
+  find "{{hdpath}}/downloads" -mindepth 2 -type f -cmin +$cleaner  $(printf "! -name %s " $(cat /opt/pgclone/functions/exclude)) -size +1M -exec rm -rf {} \;
+
+done </var/plexguide/.blitzfinal
+}
+
+# keeps the function in a loop
+cheeseballs=0
+while [[ "$cheeseballs" == "0" ]]; do startscript; done
