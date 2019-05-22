86acf08ad52255a60d0767f1e3389a19f8fd842e
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-05-22 21:13:34 +0200

    Update pgblitz.sh

--- pgblitz.sh
+++ pgblitz.sh
@@ -2,6 +2,7 @@
 #
 # Title:      PGBlitz (Reference Title File)
 # Author(s):  Admin9705 & PhysK
+# Modded :    MrDoob
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
@@ -10,8 +11,8 @@
 touch /var/plexguide/logs/pgblitz.log
 
 # Repull excluded folder 
- wget -qN https://raw.githubusercontent.com/PGBlitz/PGClone/v8.6/functions/exclude -P /opt/pgclone/functions/
-
+wget -qN https://raw.githubusercontent.com/PGBlitz/PGClone/v8.6/functions/exclude -P /var/plexguide/
+ 
 echo "" >> /var/plexguide/logs/pgblitz.log
 echo "" >> /var/plexguide/logs/pgblitz.log
 echo "----------------------------" >> /var/plexguide/logs/pgblitz.log
@@ -20,6 +21,7 @@ chown -R 1000:1000 "{{hdpath}}/downloads"
 chmod -R 775 "{{hdpath}}/downloads"
 chown -R 1000:1000 "{{hdpath}}/move"
 chmod -R 775 "{{hdpath}}/move"
+
 startscript () {
 while read p; do
 
@@ -39,6 +41,8 @@ while read p; do
   --min-age 2m \
   --fast-list \
   --update \
+  --quiet \
+  --delete-empty-src-dirs \
   --exclude-from="/opt/appdata/plexguide/exclude/exclude-file.txt"
 
   chown -R 1000:1000 "/mnt/move"
@@ -55,26 +59,16 @@ while read p; do
   --max-size=300G \
   --user-agent="$useragent" \
   --drive-chunk-size={{vfs_dcs}}M \
+  --delete-empty-src-dirs \
   --exclude-from="/opt/appdata/plexguide/exclude/exclude-file.txt"
 
   echo "Cycle $cyclecount - Sleeping for 30 Seconds" >> /var/plexguide/logs/pgblitz.log
   cat /var/plexguide/logs/pgblitz.log | tail -200 > /var/plexguide/logs/pgblitz.log
   #sed -i -e "/Duplicate directory found in destination/d" /var/plexguide/logs/pgblitz.log
   sleep 30
-  echo "Cycle $cyclecount - Sleeping for 30 Seconds" >> /var/plexguide/logs/pgblitz.log
-  cat /var/plexguide/logs/pgblitz.log | tail -200 > /var/plexguide/logs/pgblitz.log
-  #sed -i -e "/Duplicate directory found in destination/d" /var/plexguide/logs/pgblitz.log
-  sleep 30
-
-  #Quick fix
-  # Remove empty directories
-  #find "$dlpath/downloads/" -mindepth 2 -type d -empty -exec rm -rf {} \;
-  #find "$dlpath/move/" -type d -empty -exec rm -rf {} \;
-  #find "$dlpath/move/" -mindepth 2 -type f -cmin +5 -size +1M -exec rm -rf {} \;
 
   # Remove empty directories
   find "{{hdpath}}/move/" -mindepth 2 -type d -mmin +2 -empty -exec rm -rf {} \;
-
   # Removes garbage | torrent folder excluded 
   find "{{hdpath}}/downloads" -mindepth 2 -type d -cmin +$cleaner  $(printf "! -name %s " $(cat /opt/pgclone/functions/exclude)) -empty -exec rm -rf {} \;
   find "{{hdpath}}/downloads" -mindepth 2 -type f -cmin +$cleaner  $(printf "! -name %s " $(cat /opt/pgclone/functions/exclude)) -size +1M -exec rm -rf {} \;
